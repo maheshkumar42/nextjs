99dbae60699216a1fe1573354be9410c666b64b6
Mahesh Kumar <maheshkumar@Maheshs-Mini.hitronhub.home>
2023-08-03 23:25:50 -0400

    Add the About basics

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Hello world</h1>
+      <Link href='/about'>Go to About Page</Link>
     </main>
-  )
+  );
 }
